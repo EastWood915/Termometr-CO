f11704d69f7f866a1a2e46734f924b48e8c18e46
EastWood915 <wojtas1993@gmail.com>
2020-12-22 21:06:57 +0100

    Created basic project structure. Started work on Dweeter module.

---
+++ components/Dweeter/Dweeter.h
@@ -0,0 +1,6 @@
+#ifndef _DWEETER_H_
+#define _DWEETER_H_
+
+void d_http_get_task(void);
+
+#endif
---
+++ main/main.c
@@ -0,0 +1,35 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "protocol_examples_common.h"
+
+#include "Dweeter.h"
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK( nvs_flash_init() );
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    //xTaskCreate(&d_http_get_task, "http_get_task", 4096, NULL, 5, NULL);
+    d_http_get_task();
+}
